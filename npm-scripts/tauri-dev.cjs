497260882b4bd53803e083fc01e39e81ff485bbb
xclhove <xclhove@qq.com>
2026-05-16 15:48:03 +0800

    refactor(scripts): 删除构建和开发脚本中清理 frp-doc 目录的操作

--- npm-scripts/tauri-dev.cjs
+++ npm-scripts/tauri-dev.cjs
@@ -1,9 +1,5 @@
 const child_process = require('child_process')
 const { PROJECT_DIR } = require('./commons/vars.cjs')
-const fs = require('fs')
-const path = require('path')
-
-fs.rmSync(path.resolve(PROJECT_DIR, 'src-tauri/target/debug/resources/frp-doc'), { recursive: true, force: true })
 
 child_process.execSync('npm run tauri dev', {
   stdio: 'inherit',
